30c11a8849c9317c97025934365659d664a3892d
GraffAir <123460802+GraffAir@users.noreply.github.com>
2025-09-26 19:34:45 +0200

    feat(portfolio): Fond étoiles en parallax

---
+++ src/components/star.js
@@ -0,0 +1,24 @@
+'use client'
+
+import styles from './star.module.css';
+import Image from 'next/image';
+import {Parallax, ParallaxBanner, ParallaxBannerLayer, useParallax} from "react-scroll-parallax";
+
+export default function Star({ big, posX, posY, parallax, size = 20, className = '' }) {
+    console.log(posX, posY);
+    return (
+
+            <ParallaxBannerLayer speed={parallax}>
+                <Image
+                    src={big ? "/BigStar.svg" : "/SmallStar.svg"}
+                    alt=""
+                    width={size}
+                    height={size}
+                    className={`${styles.star} ${className}`}
+                    style={{left:`${posX}`, top:`${posY}`}}
+                />
+            </ParallaxBannerLayer>
+
+
+    );
+}
