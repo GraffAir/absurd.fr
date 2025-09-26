31c77dde2579e634569980a79108b5205c461656
GraffAir <123460802+GraffAir@users.noreply.github.com>
2025-09-26 19:22:44 +0200

    Configurations

--- src/components/Star.js
+++ src/components/Star.js
@@ -2,14 +2,9 @@ import styles from './Star.module.css';
 import Image from 'next/image';
 
 export default function Star({ big, size = 40, className = '' }) {
-    var image = "/SmallStar.svg";
-    if (big){
-        image = "/BigStar.svg";
-    }
-
     return (
         <Image
-            src={image}  // Place your SVG or image in the public folder
+            src={big ? "/BigStar.svg" : "/SmallStar.svg"}
             alt="star"
             width={size}
             height={size}
