40c6ee60c6318a908ae1821ba060dee3d24e9b36
GraffAir <humainabsurde@proton.me>
2025-10-21 13:51:10 +0200

    feat: debut project card

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -2,13 +2,14 @@
 
 import styles from './portfolio.module.css';
 import StarCanvas from "../../components/star.js";
+import ProjectCard from "@/components/portfolio/projectCard";
 
 export default function Page() {
 
     return (
         <main className={styles.container}>
             <h1 className={styles.titre}>machin accroche qui<br/> mentionne absurde</h1>
-            <div className={styles.mainFrame}></div>
+            <ProjectCard />
         <StarCanvas />
         </main>
     )
