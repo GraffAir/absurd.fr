8d18859b36ef4035f829df3ebc7c324e4f61c83b
GraffAir <123460802+GraffAir@users.noreply.github.com>
2025-11-10 13:22:25 +0100

    feat(portfolio): ajout variations étoiles (rouge / bleu)

--- src/components/star.js
+++ src/components/star.js
@@ -13,7 +13,7 @@ export function Star({ big, posX, posY, parallax, size = 20, className = '' }) {
                     alt=""
                     width={big ? size : size/2}
                     height={big ? size : size/2}
-                    className={`${styles.star} ${className}`}
+                    className={`${styles.star} ${className} ${Math.random() < 0.5 ? styles.starVariation1 : styles.starVariation2}`}
                     style={{left:`${posX}`, top:`${posY}`}}
                 />
             </ParallaxBannerLayer>
@@ -22,14 +22,14 @@ export function Star({ big, posX, posY, parallax, size = 20, className = '' }) {
 
 export default function StarCanvas(){
     let starList =  [];
-    const sizeMultiplier = 7;
-    for (let i = 0; i < 100; i++){
+    const sizeMultiplier = 3;
+    for (let i = 0; i < 150; i++){
         let size = Math.random()*sizeMultiplier;
         let max_size = sizeMultiplier*0.5;
         starList.push(
             <Star
                 big={size > max_size}
-                size={size}
+                size={size > sizeMultiplier*0.9 ? size*2 : size}
                 posX={`${Math.random()*100}%`}
                 posY={`${Math.random()*100}%`}
                 parallax={Math.random()*size*20}
