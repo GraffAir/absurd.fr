eb90762953855258448a3367c88b859a76587921
GraffAir <123460802+GraffAir@users.noreply.github.com>
2025-11-12 03:41:40 +0100

    feat: Page d'accueil plus épurée

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -5,6 +5,7 @@ import StarCanvas from "../../components/star.js";
 import ProjectCanvas from "@/components/portfolio/projectCanvas";
 import Planet1 from '@/components/background/Planet1.svg';
 import Nebulae2 from '@/components/background/Nebulae2.svg'
+import Link from "next/link";
 
 export default function Page() {
 
@@ -16,10 +17,24 @@ export default function Page() {
                 <Nebulae2 className={styles.nebuleuse}/>
             </div>
 
-            <h1 className={styles.titre}>machin accroche qui<br/> mentionne absurde</h1>
-            <ProjectCanvas />
+            <section className={styles.fullSection}>
+                <div className={styles.titre}>
+                    <p>Rafaël Graff</p>
+                    <h1>Créer du sens<br/> dans l&apos;absurde</h1>
+                </div>
 
+                <Link href="#description" className={styles.startButton}>Découvrir mon univers</Link>
+            </section>
+            <section id="description">
+                <h2>Description</h2>
+                <p>Bla bla du texte</p>
+            </section>
+            <section id= "productions">
+                <h2>Productions</h2>
+                <ProjectCanvas/>
+            </section>
+            <Link href="https://absurd.fr">Découvrir mon univers</Link>
 
         </main>
-    )
+    );
 }
