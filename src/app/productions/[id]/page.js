8a9932f219cc6bce57cf236fb99a18c2022f659a
GraffAir <123460802+GraffAir@users.noreply.github.com>
2025-12-21 12:33:20 +0100

    feat(productions): header des productions

--- src/app/productions/[id]/page.js
+++ src/app/productions/[id]/page.js
@@ -1,12 +1,26 @@
 import {getProductionFromId} from "@/utils/productions";
+import styles from './production.module.css';
 
 export default async function Page({params}){
     const { id } = await params;
     const prod = await getProductionFromId(id);
     return (<>
-        <header>
-            <h2>{prod.titre}</h2>
+        <header className={styles.header}>
+
         </header>
-        <main dangerouslySetInnerHTML={{__html: prod.contentHtml}} />
+        <main>
+            <div className={styles.titleContainer}>
+                <div className="flex justify-between">
+                    <p>{prod.type}</p>
+                    <p>{prod.statut}</p>
+                </div>
+
+                <h1>{prod.titre}</h1>
+                <p className={styles.desc}>{prod.description}</p>
+            </div>
+
+            <div className={styles.prodContent} dangerouslySetInnerHTML={{__html: prod.contentHtml}} />
+        </main>
+
     </>)
 }
